a74f2489af3c461e174285db10287f93a5cc9f3c
1532716 <1532716@lq3516-13.cegeplimoilou.lan>
2019-09-12 09:06:02 -0400

    ergwg42t244

--- HTML5Application/public_html/script.js
+++ HTML5Application/public_html/script.js
@@ -26,42 +26,11 @@ var context = canvas.getContext("2d");
 
 var spawnZone = canvas.style.height;
 
-var etoiles = [];
-spawnEtoile();
-
-
-var stageWidth = 300;
-var stageHeight = 150;
-var starWidth = 10;
-var starHeight = 15;
-
 var stars = [];
 
 
 function spawnEtoile(){
-    var length = 10;
-    context.translate(15,20);
-    context.rotate((Math.PI * 1 / 10));
-    for (var i = 5; i--;) {
-    context.lineTo(0, length);
-    // move origin to current same location as pen
-    context.translate(0, length);
-    // rotate the drawing board
-    context.rotate((Math.PI * 2 / 10));
-    // draw line down
-    context.lineTo(0, -length);
-    // again, move origin to pen...
-    context.translate(0, -length);
-    // ...and rotate, ready for next arm
-    context.rotate(-(Math.PI * 6 / 10));
-    }
-    context.lineTo(0, length);
-    context.closePath();
-// stroke the path, you could also .fill()
-    context.stroke();
-    
-  // console.log('ben');
-   
+
     var randomXPosition = Math.floor(Math.random() * (stageWidth - starWidth)) + 1;
     var randomYPosition = Math.floor(Math.random() * (stageHeight - starHeight)) + 1;
      
